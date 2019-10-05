13d23e36488becd1e89a430c7e468ffe59a3737d
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2019-10-05 11:54:26 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'Contador de Pessoas', home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _Pessoas = 0;
+
+  void _AtualizaPessoas(int Pessoas) {
+    setState(() {
+      _Pessoas += Pessoas;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000.0),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Pessoas: $_Pessoas',
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text('+1',
+                        style: TextStyle(fontSize: 40, color: Colors.white)),
+                    onPressed: () {
+                      _AtualizaPessoas(1);
+                    }, // Função anonima
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text('-1',
+                        style: TextStyle(fontSize: 40, color: Colors.white)),
+                    onPressed: () {
+                      _AtualizaPessoas(-1);
+                    }, // Função anonima
+                  )),
+            ]),
+            Text('Pode Entrar',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30))
+          ],
+        )
+      ],
+    );
+  }
+}
